fe0447e1c19393c02b1f48bc2f7b714746aa20fe
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2023-06-01 04:58:17 -0700

    created path formatter function

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import numpy as np
 from PIL import Image
+
+print("Input the path of the file you want to convert")
+path = input(">")
+def path_formatter(path):
+    arr = path.split('\\')
+    first_element = arr[0]
+    Remove_first = arr.pop(0) # this code will remove the first element form array b/c we need to split it again
+    first_element = first_element.split(':')
+    arr = []
+    for i in arr:
+        arr.append(f'\\\{i}')    
+    separate_elements = ''
+    output = f'{first_element[0]}:\\\{first_element[1]}{separate_elements.join(sad)}'
+    return output
+path_formatter(path)
 def convert_text_to_rgb(file):
     binary = []
     with open(file)as content:
@@ -15,9 +30,6 @@ def convert_text_to_rgb(file):
     binary = np.array(binary)
     reshape = binary.reshape(-1,3)
     return reshape 
-arr = np.array(convert_text_to_rgb("C:\\Users\\Bisrat worku\\Desktop\\rest.txt"))
+arr = np.array(convert_text_to_rgb(path_formatter))
 img = Image.fromarray(arr.astype('uint8')).convert('RGB')
-img.save('img.gif')
-
-img = Image.open('img.png')
-img.save('com.png', optimize= True, quality = 50)
+img.save('img.png')
